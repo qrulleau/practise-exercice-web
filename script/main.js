ae72e60ed80e078bc16a5f1d6a06d0f8bfe451eb
qrulleau <quentin.rulleau@yahoo.com>
2021-12-13 13:54:49 +0100

    ADD: comment and remove console.log

--- script/main.js
+++ script/main.js
@@ -3,15 +3,15 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.secondary-nav');
 
 // afficher le contenu des inputs
-
 const input = document.querySelectorAll('.display-content');
-console.log(input);
 
+// ajoute is-active au click a l element hambuger et toogle la classe display-nav a l element
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('is-active');
   nav.classList.toggle('display-nav');
 });
 
+// toogle la classe selected a l element input et il va chercher l element qui a la meme id que la value du button
 input.forEach((element) => {
   element.addEventListener('click', function () {
     element.classList.toggle('selected');
